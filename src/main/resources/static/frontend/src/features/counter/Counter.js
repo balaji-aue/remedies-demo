942599122fc8170d7661b0c05d6b5239a027b2f7
balaji <balaji@pickyourtrail.com>
2020-04-29 10:37:15 +0530

    Fetch button

--- src/main/resources/static/frontend/src/features/counter/Counter.js
+++ src/main/resources/static/frontend/src/features/counter/Counter.js
@@ -10,50 +10,27 @@ import {
 import styles from './Counter.module.css';
 
 export function Counter() {
+
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const getapivalue = ()=>fetch(`./button/fetch`).then(response=>response.json()).then(res=>{
+    dispatch(setIncrementAmount(res.data))
+  })
 
   return (
     <div>
       <div className={styles.row}>
         <button
           className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
           onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+            getapivalue()
           }
         >
-          Add Amount
+          Fetch value
         </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
+        <div>Api value here:{incrementAmount}</div>
       </div>
     </div>
   );
